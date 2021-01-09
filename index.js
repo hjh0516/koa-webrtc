ad137e0d500402f6573a0aa75fc0d680d737cc30
thomas <hjh8684@gmail.com>
2021-01-09 18:11:34 +0900

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var os = require('os');
 var nodeStatic = require('node-static');
-//var http = require('http');
+var http = require('http');
 var socketIO = require('socket.io');
 
 const https = require('https');
@@ -14,7 +14,7 @@ const options = {
 };
 
 var fileServer = new(nodeStatic.Server)();
-let app = https.createServer(options, (req,res)=>{
+let app = http.createServer(options, (req,res)=>{
   fileServer.serve(req, res);
 }).listen(3030);
 
