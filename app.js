44519ab7665302ce8d9c7d68b92af048ee8834af
thomas <hjh8684@gmail.com>
2021-01-13 16:44:27 +0900

    update

--- app.js
+++ app.js
@@ -22,15 +22,15 @@ io.on('connection', socket => {
     if (!users[socket.id]) {
         users[socket.id] = socket.id;
     }
-    socket.emit("conectedUser", socket.id);
-    io.sockets.emit("allUsers", users);
+    socket.emit('yourID', socket.id);
+    io.sockets.emit('allUsers', users);
     socket.on('disconnect', () => {
         delete users[socket.id];
     })
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit('handshake', {signal: data.signalData, from: data.from});
+    socket.on('callUser', (data) => {
+        io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
     })
-    socket.on("acceptCall", (data) => {
+    socket.on('acceptCall', (data) => {
         io.to(data.to).emit('callAccepted', data.signal);
     })
 });
