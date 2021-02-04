a49733dbd7db97b3f948b4e593428cd8031a510c
thomas <hjh8684@gmail.com>
2021-02-04 19:24:37 +0900

    add insert

--- app.js
+++ app.js
@@ -102,45 +102,35 @@ io.on('connection', socket => {
         if (users[data.to].type == 'user'){
             setDomain(Config.database, true);
             const room_no = users[data.to].room_no;
-            const check_qry = `SELECT AES_DECRYPT(unhex(?), ?) as room_no;`;
-            const check_condition = [room_no, 'dolbodaWebRTC'];
-            database.excutReader(check_qry, check_condition).then((res) => {
-                if(res[0].room_no === null){
-                    console.log('wrong room number');
-                }else{
-                    const descrypt_room_no = res[0].room_no.toString();
-                    const [domain, reserve_no] = descrypt_room_no.split('_');
-                    setDomain(domain);
+            const [domain, reserve_no] = room_no.split('_');
+            setDomain(domain);
 
-                    const check_diagnosis = `SELECT 1 FROM diagnosis WHERE reserve_id = ?;`;
-                    database.excutReader(check_diagnosis, [reserve_no]).then((res) => {
-                        if(res == null){
-                            const insert_qry = `INSERT INTO diagnosis (reserve_id, user_id, account_id, room_no, 
-                                type, started_at, status) 
-                                SELECT r.id, r.user_id, r.account_id, ?, 1, now(), 1 FROM reserves r
-                                    LEFT OUTER JOIN diagnosis d ON d.reserve_id = r.id 
-                                WHERE r.id = ? AND d.id IS NULL;
-                            `;
-                            const insert_conditions = [descrypt_room_no, reserve_no];
-                            database.excutNonQuery(insert_qry, insert_conditions);
-                        }
-                    });
+            const check_diagnosis = `SELECT 1 FROM diagnosis WHERE reserve_id = ?;`;
+            database.excutReader(check_diagnosis, [reserve_no]).then((res) => {
+                if(res == null){
+                    const insert_qry = `INSERT INTO diagnosis (reserve_id, user_id, account_id, room_no, 
+                        type, started_at, status) 
+                        SELECT r.id, r.user_id, r.account_id, ?, 1, now(), 1 FROM reserves r
+                            LEFT OUTER JOIN diagnosis d ON d.reserve_id = r.id 
+                        WHERE r.id = ? AND d.id IS NULL;
+                    `;
+                    const insert_conditions = [descrypt_room_no, reserve_no];
+                    database.excutNonQuery(insert_qry, insert_conditions);
                 }
-            }).catch((err)=>{
-                console.log(err);
             });
         }
     });
 
     socket.on("callOff", (data) => {
+        console.log(data);
         if (rooms[room_no] != undefined){
             console.log('disconnect one');
 
-            const update_qry = `UPDATE diagnosis SET ended_at
-                WHERE reserve_id = ? AND ended_at IS NULL;
-            `;
-            const update_conditions = [descrypt_room_no, reserve_no];
-            database.excutNonQuery(update_qry, update_conditions);
+            // const update_qry = `UPDATE diagnosis SET ended_at
+            //     WHERE reserve_id = ? AND ended_at IS NULL;
+            // `;
+            // const update_conditions = [descrypt_room_no, reserve_no];
+            // database.excutNonQuery(update_qry, update_conditions);
             if (rooms[room_no].first_socket_id == data.from.id){
                 rooms[room_no].first_socket_id = null
                 io.to(rooms[room_no].second_socket_id).emit('connectedUsers', null);
